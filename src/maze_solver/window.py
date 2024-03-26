b87f5d2ac2e19d4d8871d28c77a82c805a24bcb2
William Wingate <wtwingate@gmail.com>
2024-03-26 18:53:34 -0400

    Make the state of the Window object private

--- src/maze_solver/window.py
+++ src/maze_solver/window.py
@@ -3,21 +3,21 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height) -> None:
-        self.root = Tk()
-        self.root.title("Maze Solver")
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas(self.root, width=width, height=height)
-        self.canvas.pack()
-        self.running = False
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack()
+        self.__running = False
 
     def redraw(self) -> None:
-        self.root.update_idletasks()
-        self.root.update()
+        self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self) -> None:
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
 
     def close(self) -> None:
-        self.running = False
+        self.__running = False
