92521d77dbc422ba7d0a700adfd56fe124421bae
William Wingate <wtwingate@gmail.com>
2024-03-26 19:27:07 -0400

    Rename file and add Point and Line classes
    We can draw a line on our Window object now! Now we're cooking with gas!

---
+++ src/maze_solver/graphics.py
@@ -0,0 +1,44 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack()
+        self.__running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+
+    def close(self):
+        self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x_coord, y_coord):
+        self.x = x_coord
+        self.y = y_coord
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.__p1 = point1
+        self.__p2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y, fill=fill_color, width=2
+        )
+        canvas.pack()
--- src/maze_solver/main.py
+++ src/maze_solver/main.py
@@ -1,8 +1,12 @@
-from window import Window
+from graphics import Window, Point, Line
 
 
 def main():
     window = Window(800, 600)
+    point1 = Point(200, 500)
+    point2 = Point(500, 200)
+    line = Line(point1, point2)
+    window.draw_line(line, "black")
     window.wait_for_close()
 
 
