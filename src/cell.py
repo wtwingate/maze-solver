e708f7a045a88a849cd53b23fc09708da3b40c24
William Wingate <wtwingate@gmail.com>
2024-03-26 23:44:13 -0400

    Write unit tests for Maze class

--- src/cell.py
+++ src/cell.py
@@ -8,41 +8,45 @@ class Cell:
         self.south_wall = True
         self.east_wall = True
         self.west_wall = True
-        self.__window = window
+        self._window = window
         # (x1, y1) -> top-left point
-        self.__x1 = x1
-        self.__y1 = y1
+        self._x1 = x1
+        self._y1 = y1
         # (x2, y2) -> bottom-right point
-        self.__x2 = x2
-        self.__y2 = y2
+        self._x2 = x2
+        self._y2 = y2
 
     def draw(self):
+        if self._window is None:
+            return
         if self.north_wall:
-            self.__window.draw_line(
-                Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)),
+            self._window.draw_line(
+                Line(Point(self._x1, self._y1), Point(self._x2, self._y1)),
                 WALL_COLOR,
             )
         if self.south_wall:
-            self.__window.draw_line(
-                Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)),
+            self._window.draw_line(
+                Line(Point(self._x1, self._y2), Point(self._x2, self._y2)),
                 WALL_COLOR,
             )
         if self.east_wall:
-            self.__window.draw_line(
-                Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)),
+            self._window.draw_line(
+                Line(Point(self._x2, self._y1), Point(self._x2, self._y2)),
                 WALL_COLOR,
             )
         if self.west_wall:
-            self.__window.draw_line(
-                Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)),
+            self._window.draw_line(
+                Line(Point(self._x1, self._y1), Point(self._x1, self._y2)),
                 WALL_COLOR,
             )
 
     def center_point(self):
-        return Point((self.__x1 + self.__x2) / 2, (self.__y1 + self.__y2) / 2)
+        return Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2)
 
     def draw_move(self, other, undo=False):
-        self.__window.draw_line(
+        if self._window is None:
+            return
+        self._window.draw_line(
             Line(self.center_point(), other.center_point()),
             UNDO_COLOR if undo else PATH_COLOR,
         )
--- src/tests.py
+++ src/tests.py
@@ -1,1 +1,36 @@
 import unittest
+from maze import Maze
+
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 12
+        maze = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(len(maze._cells), num_cols)
+        self.assertEqual(len(maze._cells[0]), num_rows)
+
+    def test_large_maze_create_cells(self):
+        num_cols = 100
+        num_rows = 100
+        maze = Maze(100, 100, num_rows, num_cols, 100, 100)
+        self.assertEqual(len(maze._cells), num_cols)
+        self.assertEqual(len(maze._cells[0]), num_rows)
+
+    def test_wide_maze_create_cells(self):
+        num_cols = 48
+        num_rows = 12
+        maze = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(len(maze._cells), num_cols)
+        self.assertEqual(len(maze._cells[0]), num_rows)
+
+    def test_tall_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 48
+        maze = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(len(maze._cells), num_cols)
+        self.assertEqual(len(maze._cells[0]), num_rows)
+
+
+if __name__ == "__main__":
+    unittest.main()
