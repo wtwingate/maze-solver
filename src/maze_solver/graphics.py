9d45fda49fe0223f59ca9c5eeac2189913b23411
William Wingate <wtwingate@gmail.com>
2024-03-26 21:08:31 -0400

    Create Cell class and its draw method

--- src/maze_solver/graphics.py
+++ src/maze_solver/graphics.py
@@ -1,12 +1,15 @@
 from tkinter import Tk, BOTH, Canvas
 
+WALL_COLOR = "black"
+PATH_COLOR = "red"
+
 
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
         self.__canvas.pack()
         self.__running = False
 
@@ -42,3 +45,40 @@ class Line:
             self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y, fill=fill_color, width=2
         )
         canvas.pack()
+
+
+class Cell:
+    def __init__(self, window, x1, y1, x2, y2):
+        self.north_wall = True
+        self.south_wall = True
+        self.east_wall = True
+        self.west_wall = True
+        self.__window = window
+        # (x1, y1) -> top-left point
+        self.__x1 = x1
+        self.__y1 = y1
+        # (x2, y2) -> bottom-right point
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def draw(self):
+        if self.north_wall:
+            self.__window.draw_line(
+                Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)),
+                WALL_COLOR,
+            )
+        if self.south_wall:
+            self.__window.draw_line(
+                Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)),
+                WALL_COLOR,
+            )
+        if self.east_wall:
+            self.__window.draw_line(
+                Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)),
+                WALL_COLOR,
+            )
+        if self.west_wall:
+            self.__window.draw_line(
+                Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)),
+                WALL_COLOR,
+            )
--- src/maze_solver/main.py
+++ src/maze_solver/main.py
@@ -1,12 +1,18 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 
 def main():
     window = Window(800, 600)
-    point1 = Point(200, 500)
-    point2 = Point(500, 200)
-    line = Line(point1, point2)
-    window.draw_line(line, "black")
+
+    cell1 = Cell(window, 200, 200, 300, 300)
+    cell2 = Cell(window, 300, 300, 400, 400)
+    cell1.north_wall = False
+    cell1.south_wall = False
+    cell2.east_wall = False
+    cell2.west_wall = False
+    cell1.draw()
+    cell2.draw()
+
     window.wait_for_close()
 
 
